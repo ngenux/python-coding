68fd58563a89450ba2d9cb8ebeb9d764cd7ecd9a
Tanu Behera <Tanusobhan.Behera@ngenux.com>
2023-02-07 13:58:07 +0530

    Added showing  available cells

--- Team_Harmony/tictactoe.py
+++ Team_Harmony/tictactoe.py
@@ -7,6 +7,7 @@ empty_cell = '| |'
 # This function maps the location entered by the Players to the 2-D board. 
 # 1 is represented as [0,0] till 9 represented as [2,2]
 def loc_map(loc):
+    board_loc = 0
     if loc == 1:
         board_loc = board[0][0]
     if loc == 2:
@@ -30,9 +31,9 @@ def loc_map(loc):
 # Function for getting the list of all available cells after each Player move
 def current_available_cells():
     str_available_cell = ''
-    for i in range(8):
-        if loc_map(i) == '| |':
-            str_available_cell = str_available_cell + str(i+1)
+    for i in range(9):
+        if loc_map(i+1) == empty_cell:
+            str_available_cell = str_available_cell + str(i+1) + ','
     return (str_available_cell)
 
 # Defines the empty cells in the 2-D board
@@ -144,7 +145,9 @@ def P1_inputs(turn,board):
     try:
         # Checking if turn is for Player 1 (X)
         while (turn % 2 == 0):
-            print('Please enter number from 1 to 9')
+#             print('Please enter number from 1 to 9')
+            cellno = current_available_cells()
+            print('Available cells: ' + cellno)
             player_move = int(input("please enter move (P-1)X:").strip())
 
             validate_loc_num_input(player_move)
@@ -173,17 +176,13 @@ def P1_inputs(turn,board):
     #             boardstate(player_move,'x',0)
                 
                 print(format_board(player_move,boardstate,'x'))
-#                 print(current_available_cells())
+#                 cellno = current_available_cells()
+#                 print('Available cells: ' + cellno)
                 if win_check():
                     print('P-1 WINS')
-#                     return
-    #                 print(boardstate(player_move,'x',0))
-    #                 print(format_board(player_move,boardstate,'x'))
                     board,turn = init_state_board()
                     break
                     
-    #             print(format_board(player_move,boardstate,'x'))
-
                 turn = turn + 1
         
                 if draw_check():
@@ -193,13 +192,18 @@ def P1_inputs(turn,board):
         
             else:
                 print('Place already taken. Try again..')
+                cellno = current_available_cells()
+                print('Available cells: ' + cellno)
     except:
-        print('Please enter between 1 and 9')
+        cellno = current_available_cells()
+        print('Available cells: ' + cellno)
     return (board, turn)
 
 def P2_inputs(turn,board):
     try:
         while (turn % 2 == 1):
+            cellno = current_available_cells()
+            print('Available cells: ' + cellno)
             player_move = int(input("Please enter move (P-2)O:").strip())
             validate_loc_num_input(player_move)
 
@@ -221,17 +225,15 @@ def P2_inputs(turn,board):
                 board_loc = board[2][1]
             if player_move == 9:
                 board_loc = board[2][2]
-    #                 if player_move != [1-9]:
-    #                     print('Wrong location')
 
             if board_loc == empty_cell:
                 print(format_board(player_move,boardstate,'o'))
+
                 if win_check():
                     print('P-2 WINS')
                     board,turn = init_state_board()
                     break
-    #                 print(boardstate(player_move,'o',0))
-    #                         break
+
                 turn = turn + 1
         
                 if draw_check():
@@ -240,16 +242,19 @@ def P2_inputs(turn,board):
                     break
             else:
                 print('Place already taken. Try again..')
+                cellno = current_available_cells()
+                print('Available cells: ' + cellno)
     except:
-        print('Please enter between 1 and 9')
+        cellno = current_available_cells()
+        print('Available cells: ' + cellno)
     return (board, turn)
     
 # ------------------------------------------------------------------------
 # Start main
 new_game ='y'
-while new_game == 'y':
-    new_game = str(input("Do you want to start a new game (y to play. press any key to exit)?: ").strip())
-    if new_game == 'y':
+while new_game == 'y' or new_game == 'Y':
+    new_game = str(input("Do you want to start a new game (y to play. press any other key to exit)?: ").strip())
+    if new_game == 'y' or new_game == 'Y':
         print('Welcome to Tic-Tac-Toe')
         print('The inputs used are represented in the board as below')
         # New game starts here
